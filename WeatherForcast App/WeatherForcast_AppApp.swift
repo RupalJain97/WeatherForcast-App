34ec5d4336e31c20d04bb2842a7cb4837e4c98d3
Rupal Jain <jainrupal9727@gmail.com>
2024-11-18 20:44:02 -0800

    Initial Commit

---
+++ WeatherForcast App/WeatherForcast_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcast_AppApp.swift
+//  WeatherForcast App
+//
+//  Created by Rupal Jain on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForcast_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherForcast AppTests/WeatherForcast_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherForcast_AppTests.swift
+//  WeatherForcast AppTests
+//
+//  Created by Rupal Jain on 11/18/24.
+//
+
+import Testing
+
+struct WeatherForcast_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
